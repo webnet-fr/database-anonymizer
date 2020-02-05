0c956e0a611a2d85d892a72e32d0b9be1f8cd328
Vlad Riabchenko <contact@vria.eu>
2020-02-05 15:28:34 +0100

    test sqlsrv

--- tests/travis/install-mssql.sh
+++ tests/travis/install-mssql.sh
@@ -2,34 +2,30 @@
 
 set -ex
 
+
+echo Installing driver dependencies
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
+sudo apt-get update
+ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 unixodbc unixodbc-dev libssl1.0.0
+
+
+echo Installing extension
+pecl install sqlsrv-5.7.0preview
+
+
+echo Setting up Microsoft SQL Server
+
 sudo docker pull microsoft/mssql-server-linux:2017-latest
 sudo docker run \
     -e 'ACCEPT_EULA=Y' \
-    -e 'SA_PASSWORD=Anonymizer2018' \
+    -e 'SA_PASSWORD=pass' \
     -p 127.0.0.1:1433:1433 \
-    --name db \
+    --name mssql \
     -d \
     microsoft/mssql-server-linux:2017-latest
 
-
-retries=20
-until (echo quit | /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -l 1 -U sa -P Anonymizer2018 &> /dev/null)
-do
-    if [[ "$retries" -le 0 ]]; then
-        echo SQL Server did not start
-        exit 1
-    fi
-
-    retries=$((retries - 1))
-
-    echo Waiting for SQL Server to start...
-
-    sleep 5s
-done
+sudo docker exec -i mssql bash <<< 'until echo quit | /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -l 1 -U sa -P pass > /dev/null 2>&1 ; do sleep 1; done'
 
 echo SQL Server started
-
-
-echo "Installing extension"
-pecl channel-update pecl.php.net
-pecl install sqlsrv
