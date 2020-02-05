732ab9680f23c82e19a83f179e49929a2f90272f
Vlad Riabchenko <contact@vria.eu>
2020-02-05 14:54:59 +0100

    correct sqlsrv travis test

--- tests/travis/install-mssql.sh
+++ tests/travis/install-mssql.sh
@@ -3,6 +3,7 @@
 set -ex
 
 echo Installing drivers
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
 curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
